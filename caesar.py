81dda3c0d06f73b81e155929762c7f329270466f
AyeshaAshraf-dev <ayeshaashraf171@gmail.com>
2026-01-02 21:51:30 +0500

    I modified my previos code

--- caesar.py
+++ caesar.py
@@ -1,5 +1,4 @@
-
-def caesar(text, shift):
+def caesar(text, shift, encrypt=True):
     if not isinstance(shift, int):
         return 'Shift must be an integer value.'
 
@@ -7,24 +6,19 @@ def caesar(text, shift):
         return 'Shift must be an integer between 1 and 25.'
 
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    if not encrypt:
+        shift = - shift
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
-    return text.translate(translation_table)
+    encrypted_text = text.translate(translation_table)
+    return encrypted_text
 
-
-encrypted_text = caesar('freeCodeCamp', 3)
-def caesar(text, shift):
-    if not isinstance(shift, int):
-        return 'Shift must be an integer value.'
-
-    if shift < 1 or shift > 25:
-        return 'Shift must be an integer between 1 and 25.'
-
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
-    return text.translate(translation_table)
-
-
-encrypted_text = caesar('freeCodeCamp', 3)
-print(encrypted_text)
+def encrypt(text, shift):
+    return caesar(text, shift)
+    
+def decrypt(text, shift):
+    return caesar(text, shift, encrypt=False)
+encrypted_text = "Pbhentr vf sbhaq va hayvxryl cynprf."
+decrypted_text = decrypt(encrypted_text, 13)
+print(decrypted_text)
